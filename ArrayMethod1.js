8bc4be2041496c6b373e59e94561ace83f68fb2d
Shubham <shivam306joshi@gmail.com>
2023-08-11 11:48:37 +0530

    first commit

---
+++ ArrayMethod1.js
@@ -0,0 +1,13 @@
+// What will be the output of the below code:
+
+// const array = [10, 20, 30, 40];
+// const result = array.map((num) => num / 2).filter((num) => num >= 15);
+// console.log(result);
+
+
+
+const array = [10, 20, 30, 40];
+const result = array.map((num) => num / 2).filter((num) => num >= 15);
+console.log(result);
+
+// map method will add each element of array by 2 and filter method will return the element greater than or equal to 15.
