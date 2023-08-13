562d6b715b01b1141b98df7ce5e099ffc8553fb5
Shubham <shivam306joshi@gmail.com>
2023-08-13 18:52:57 +0530

    How do you calculate the sum of two integers?

---
+++ SumOfNum.js
@@ -0,0 +1,21 @@
+// How do you calculate the sum of two integers?
+
+const num1 = 5;
+const num2 = 7;
+
+const sum = num1 + num2;
+
+console.log("Sum:", sum);
+
+// Another Way
+
+function calculateSum(a, b) {
+    return a + b;
+  }
+  
+  const num3 = 5;
+  const num4 = 7;
+  
+  const sum5 = calculateSum(num1, num2);
+  
+  console.log("Sum:", sum);
