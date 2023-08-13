583a53fd32699e3c7d1cbbddfc85a32603aa538a
Shubham <shivam306joshi@gmail.com>
2023-08-13 17:08:43 +0530

    How do you sort an array of integers in ascending order?

---
+++ AscendingArray.js
@@ -0,0 +1,7 @@
+// How do you sort an array of integers in ascending order?
+
+const numbers = [23, 56, 12, 8, 45, 1, 67];
+
+numbers.sort((a, b) => a - b);
+
+console.log(numbers); // Output: [1, 8, 12, 23, 45, 56, 67]
